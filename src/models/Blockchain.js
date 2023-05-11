3f38262ff76d17ae3c1595d0b557cfd971da5a4f
chuanyue <2399147152@cy.com>
2023-05-11 23:22:11 +0800

    first commit

--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,22 +1,32 @@
-// Blockchain
 class Blockchain {
   // 1. 完成构造函数及其参数
-  /* 构造函数需要包含 
-      - 名字
-      - 创世区块
-      - 存储区块的映射
-  */
-  constructor() {}
+  /* 构造函数需要包含 - 名字 - 创世区块 - 存储区块的映射 */
+  constructor(name) {
+    this.name = name;
+    this.genesis = null;
+    this.blocks = {};
+  }
 
   // 2. 定义 longestChain 函数
-  /* 
-    返回当前链中最长的区块信息列表
-  */
+  /* 返回当前链中最长的区块信息列表 */
   longestChain() {
+    let longestChain = []
+    let nextBlocks = Object.values(this.blocks)
+    let temp = this.genesis
+    nextBlocks.forEach(function(block) {
+      if(temp.height < block.height){
+        temp = block
+      }
+    })
 
-
-    return []
+    while(temp.previousHash != "root"){
+      longestChain.push(temp)
+      temp = this.blocks[temp.previousHash]
+    }
+    longestChain.push(temp)
+    longestChain.reverse()
+    return longestChain;
   }
 }
 
-export default Blockchain
+export default Blockchain;
