c7b23a3f83d8301218ea7edf97ff0177f551293d
Kiritokuto <101487263+Kiritokuto@users.noreply.github.com>
2023-05-17 23:51:58 +0800

    Update Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,12 +1,30 @@
+import sha256 from 'crypto-js/sha256.js'
+export const DIFFICULTY = 3
+
 class Block {
   // 1. 完成构造函数及其参数
-  /* 构造函数需要包含 */
-  constructor(blockchain, previousHash, height, hash) {
-    this.blockchain = blockchain;
-    this.previousHash = previousHash;
-    this.height = height;
-    this.hash = hash;
+  constructor(blockchain,previousHash,height,hash,nonce) {
+    this.blockchain=blockchain;
+    this.previousHash=previousHash;
+    this.height=height
+    this.hash=hash
+    this.nonce=nonce
   }
+  
+  isValid() {
+    const hash=this.calculateHash()
+    return hash.substring(0,DIFFICULTY) == '0'.repeat(DIFFICULTY)
+  }
+
+  calculateHash(){
+    return sha256(
+      this.previousHash+this.height+this.nonce
+    ).toString()
+   }
+
+   //设置随机数
+  setNonce(nonce) {
+    this.nonce=nonce}
 }
 
-export default Block;
+export default Block
