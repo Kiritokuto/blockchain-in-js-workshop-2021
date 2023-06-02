6b636112e8b9b5abcbe6b5ad0f51b6f6a5bebb7c
Kiritokuto <101487263+Kiritokuto@users.noreply.github.com>
2023-06-02 23:30:00 +0800

    Create Transaction.js

---
+++ src/models/Transaction.js
@@ -0,0 +1,23 @@
+import sha256 from 'crypto-js/sha256.js'
+
+class Transaction {
+  constructor(sender, receiver, amount) {
+    this.sender = sender
+    this.receiver = receiver
+    this.amount = amount
+    this.timestamp = new Date().getTime()
+    this.hash = this._calculateHash()
+  }
+
+  _setHash() {
+    this.hash = this._calculateHash()
+  }
+
+  _calculateHash() {
+    return sha256(
+      this.sender + this.receiver + this.amount + this.timestamp
+    ).toString()
+  }
+}
+
+export default Transaction
